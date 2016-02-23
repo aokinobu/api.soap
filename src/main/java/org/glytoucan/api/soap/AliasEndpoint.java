a230e6601befd6d46b37d79a50a95941af4cfe73
aoki <aoki@bluetree.jp>
2016-02-23 18:43:45 +0900

    compiling...

---
+++ src/main/java/org/glytoucan/api/soap/AliasEndpoint.java
@@ -0,0 +1,44 @@
+package org.glytoucan.api.soap;
+
+import java.math.BigInteger;
+
+import org.glytoucan.api.soap.contributor.AliasRegisterRequest;
+import org.glytoucan.api.soap.contributor.AliasRegisterResponse;
+import org.glytoucan.api.soap.contributor.ResponseMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+@Endpoint
+public class AliasEndpoint {
+	private static final String NAMESPACE_URI = "http://soap.api.glytoucan.org/contributor";
+
+	private AliasRepository aliasRepository;
+
+	@Autowired
+	public AliasEndpoint(AliasRepository aliasRepository) {
+		this.aliasRepository = aliasRepository;
+	}
+
+	@PayloadRoot(namespace = NAMESPACE_URI, localPart = "getTrivialNameRequest")
+	@ResponsePayload
+	public AliasRegisterResponse registerAlias(@RequestPayload AliasRegisterRequest request) {
+		AliasRegisterResponse response = new AliasRegisterResponse();
+		ResponseMessage rm = new ResponseMessage();
+		rm.setMessage("SUCCESS!" + this.aliasRepository.findTrivialName(request.getTrivialName().getName()));
+		rm.setErrorCode(new BigInteger("0"));
+
+		return response;
+	}
+
+	// @ResponsePayload
+	// public AliasRegisterResponse getTrivialName(@RequestPayload
+	// TrivialNameRequest request) {
+	// ResponseMessage rm = new ResponseMessag()
+	// response.setTrivialName(trivialNameRepository.findTrivialName(request.getSequence()));
+	//
+	// return response;
+	// }
+}
---
+++ src/main/java/org/glytoucan/api/soap/AliasRepository.java
@@ -0,0 +1,47 @@
+package org.glytoucan.api.soap;
+
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.glytoucan.api.soap.contributor.NotationSchema;
+import org.glytoucan.api.soap.contributor.TrivialName;
+import org.springframework.stereotype.Component;
+import org.springframework.util.Assert;
+
+@Component
+public class AliasRepository {
+	private static final List<TrivialName> names = new ArrayList<TrivialName>();
+
+	@PostConstruct
+	public void initData() {
+		TrivialName bcsdb = new TrivialName();
+		bcsdb.setName("aD3,7dgalNonp5N-ulosonic");
+		bcsdb.setNotationSchema(NotationSchema.BCSDB);
+		names.add(bcsdb);
+
+		TrivialName msdb = new TrivialName();
+		msdb.setName("a-dgal-NON-2:6|1:a|2:keto|3:d|7:d||(5d:1)n-acetyl");
+		msdb.setNotationSchema(NotationSchema.MSDB);
+		names.add(msdb);
+
+		TrivialName carbbank = new TrivialName();
+		carbbank.setName("a-D-3,7-deoxy-galNon2ulop5NAc-onic");
+		carbbank.setNotationSchema(NotationSchema.CARBBANK);
+		names.add(bcsdb);
+	}
+
+	public TrivialName findTrivialName(String name) {
+		Assert.notNull(name);
+
+		TrivialName result = null;
+
+		for (TrivialName n : names) {
+			if (name.equals(n.getName())) {
+				result = n;
+			}
+		}
+
+		return result;
+	}
+}
