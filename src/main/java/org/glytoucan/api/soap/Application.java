f5dcadd6fa6e6188d23a2e26e91bd15f5b82e642
aoki <aokinobu@gmail.com>
2016-06-06 18:47:38 +0900

    revert to 1.3.3 spring boot

--- src/main/java/org/glytoucan/api/soap/Application.java
+++ src/main/java/org/glytoucan/api/soap/Application.java
@@ -3,15 +3,16 @@ package org.glytoucan.api.soap;
 import org.glycoinfo.rdf.dao.virt.VirtSesameTransactionConfig;
 import org.glycoinfo.rdf.service.impl.GlycanProcedureConfig;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.actuate.autoconfigure.ManagementWebSecurityAutoConfiguration;
+//import org.springframework.boot.actuate.autoconfigure.ManagementWebSecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 @Import({ GlycanProcedureConfig.class, VirtSesameTransactionConfig.class })
-@EnableAutoConfiguration(exclude = { org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration.class,
-    ManagementWebSecurityAutoConfiguration.class })
+@EnableAutoConfiguration(exclude = { SecurityAutoConfiguration.class,
+    org.springframework.boot.autoconfigure.security.SpringBootWebSecurityConfiguration.class })
 public class Application {
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
